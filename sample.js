9ffc6e113b79c83342d83053ee8d05b0fbb14518
gabiaFe <dochoul@gmail.com>
2016-12-27 16:56:00 +0900

    add sample js

---
+++ sample.js
@@ -0,0 +1,32 @@
+var untitled = untitled || (function() {
+
+	var _public = {}
+
+	var browser = function() {
+		return 'chrome';
+	}
+
+	var isMobile = function() {
+		return true;
+	}
+
+	var etQueryParams = function(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i=0;i<vars.length;i++) {
+			var pair = vars[i].split("=");
+			if(pair[0] == variable){return pair[1];}
+		}
+		return(false);
+	}
+
+	_public.browser = browser;
+	_public.isMobile = isMobile;
+	_public.etQueryParams = etQueryParams;
+
+	return _public;
+
+}());
+
+var b = untitled.etQueryParams('tabActive');
+console.log(     b     );
