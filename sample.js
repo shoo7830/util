eb4eca0aab97781e3853ffb7a131d6366780da9a
jnh0222 <jnh@gabia.com>
2016-12-29 15:43:57 +0900

    add 2 functoin.
    1. isMobile
    2. browser

--- sample.js
+++ sample.js
@@ -3,11 +3,45 @@ var untitled = untitled || (function() {
 	var _public = {}
 
 	var browser = function() {
-		return 'chrome';
+		var browserPool = [
+			{detectStr: "msie", name: "ie"},
+			{detectStr: "trident", name: "ie"},
+			{detectStr: "edge", name: "edge"},
+			{detectStr: "firefox", name: "firefox"},
+			{detectStr: "opr", name: "opera"},
+			{detectStr: "chrome", name: "chrome"},
+			{detectStr: "safari", name: "safari"}
+		];
+		var ua = navigator.userAgent.toLowerCase();
+		var n, v, vOffset;
+		for(var i in browserPool) {
+			if((vOffset = ua.indexOf(browserPool[i].detectStr)) > -1) {
+				n = browserPool[i].name;
+				if(browserPool[i].detectStr == 'trident') { //ie11
+					v = ua.substr(ua.indexOf('rv:') + 3);
+					v = parseFloat(v.split(')')[0]);
+				}
+				else if(browserPool[i].detectStr == 'safari') {
+					v = ua.substr(ua.indexOf('version/') + 8);
+					v = parseFloat(v.split('/')[0]);
+				}
+				else {
+					v = ua.substr(vOffset + browserPool[i].detectStr.length + 1);
+					v = parseFloat(v.split(' ')[0]);	
+				}
+				break;
+			}else {
+				n = "unknown";
+				v = 0;
+			}
+		}
+		return {name: n, version: v};		
 	}
 
 	var isMobile = function() {
-		return true;
+		var pfm = navigator.platform.toLowerCase();
+		var detectStr = "win16|win32|win64|mac|macintel";
+		return (detectStr.indexOf(pfm)<0) ? true : false;
 	}
 
 	var getParameterByName = function(name) {
@@ -37,8 +71,8 @@ var untitled = untitled || (function() {
         //window.open()
     }
 	
-	_public.browser = browser;
-	_public.isMobile = isMobile;
+	_public.browser = browser();
+	_public.isMobile = isMobile();
 	_public.getParameterByName = getParameterByName;
 	
 	_public.addComma = addComma;
