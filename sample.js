03275ace1d5c4dd70c7ff2f43b903825b0f0e3b4
gabiaFe <dochoul@gmail.com>
2017-01-02 17:04:37 +0900

    comment add get parameter values ​​from an URL

--- sample.js
+++ sample.js
@@ -51,17 +51,17 @@ var untitled = untitled || (function() {
     * usage: untitled.getParameterByName('parameter name');
     * ========================================================================
     */
-	var getParameterByName = function(name) {
-		if (name === undefined || name === '') return;
-		var query = window.location.search.substring(1);
-		var vars = query.split('&');
-		for (var i=0; i<vars.length; i++) {
-			var pair = vars[i].split('=');
-			if(pair[0] == name) {
-				return pair[1];
-			}
-		}
-	}
+    var getParameterByName = function(name) {
+        if (name === undefined || name === '') return;
+        var query = window.location.search.substring(1);
+        var vars = query.split('&');
+        for (var i=0; i<vars.length; i++) {
+            var pair = vars[i].split('=');
+            if(pair[0] == name) {
+                return pair[1];
+            }
+        }
+    }
 
 
 	/**
